16db1dfd90e08460d75103e081ec187588df73bf
Rose-Walker <g00438842@atu.ie>
2025-01-03 16:10:49 +0000

    implemented local storage for storing selected unit on settings page

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent  } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonRadio, IonRadioGroup  } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
-  imports: [ IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ IonHeader, IonToolbar, IonTitle, IonContent, IonRadio, IonRadioGroup, FormsModule],
 })
 export class SettingsPage {
+
+    selectedUnit: string = 'metric'; // Default value
+
   constructor() {
+  }
+
+  async ngOnInit() {
+    var savedUnit = localStorage.getItem("selectedUnit");
+    if(savedUnit)
+        this.selectedUnit = savedUnit;
+  }
+
+  async onRadioSelectedChanged(){
+    localStorage.setItem("selectedUnit", this.selectedUnit)
   }
 }
